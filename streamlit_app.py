166b9c99ab3de3fca3c4c68651b7561679cc8c9f
PREE <preemade@gmail.com>
2025-07-30 15:29:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="collapsed",
 )
+user_input = st.text_input("What's on your mind today? (mom guilt, stress, doubts, anything)")
 st.markdown("""
     <style>
     /* Input field text color fix */
