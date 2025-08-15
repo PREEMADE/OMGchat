9c73f39be24ba712b0523d2c41f3371a4b67486b
PREE <preemade@gmail.com>
2025-08-14 18:41:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ st.markdown(
     """
     <style>
     @keyframes blink-caret {
-        1%, 100% { border-right: 0.15em solid rgba(25, 178, 214, 0); }  /* Transparent */
+        50%, 100% { border-right: 0.15em solid rgba(25, 178, 214, 0); }  /* Transparent */
         50% { border-right: 0.15em solid rgba(25, 178, 214, 1); }       /* Visible */
     }
 
